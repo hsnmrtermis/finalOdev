71aac4b09b44271a6dddfcb4a7772bd628397815
Hasan Mert ERMİŞ <32934820+hsnmrtermis@users.noreply.github.com>
2020-07-02 15:44:04 +0300

    kullanici adi otomatik geliyor

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,18 +1,63 @@
 import React, { useState, useEffect } from 'react'
-import {View , Text, SafeAreaView, Button, TextInput} from 'react-native'
+import {View , Text, SafeAreaView, Button, TextInput, FlatList} from 'react-native'
 import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 import AsyncStorage from '@react-native-community/async-storage';
-const MainPage = props => {
 
+const MainPage = props => {
+    let kisiId =  auth().currentUser.uid
     const [mail , setMail] = useState("")
     const [userName, setUserName] = useState("Henüz Çevirilmedi , butona tıkla")
+    const [postList, setPostList] = useState([])
+    const [yazi, setYazi] = useState("")
+    
     useEffect(() => {
         
-      let mailadresi =   auth().currentUser._user.email
+        let mailadresi =   auth().currentUser._user.email
         setMail(mailadresi)
+        var indis = mailadresi.lastIndexOf("@")
+        var newUserName = mailadresi.slice(0,indis)
+        setUserName(newUserName)
+        getPosts()
+       
+
+                database()
+        .ref(`/${kisiId}/`)
+        .once('value')
+        .then(snapshot => {
+            console.log('Gelen: ', snapshot.val());
+        });
 
     }, [])
     
+
+    const getPosts = () => {
+        database()
+        .ref()
+        .once('value')
+        .then(snapshot => {
+            console.log('User data: ', snapshot.val());
+
+        });
+    }
+
+    const pushData = () => {
+        //var indis = mail.lastIndexOf("@")
+        //var newUserName = mail.slice(0,indis)
+        //setUserName(newUserName)
+
+        var data = {
+            username : userName,
+            yazi : yazi
+
+        }
+
+                    database()
+                    .ref(`${kisiId}`)
+                    .push(data);
+    }
+
+    const changePost = (text) => setYazi(text)
     
 
     const LogOut = () => {
@@ -20,12 +65,9 @@ const MainPage = props => {
         auth().signOut()
         props.navigation.navigate("LoginPage")
     }
+   
 
-    const ConvertUserName = () => {
-        var indis = mail.lastIndexOf("@")
-        var newUserName = mail.slice(0,indis)
-        setUserName(newUserName)
-    }
+ 
 
     return(
         <SafeAreaView>
@@ -34,11 +76,7 @@ const MainPage = props => {
             <Text style={{fontWeight:'bold'}}>Kullanıcı Adı :{userName}</Text>
             
             <View style={{marginTop:15}}></View>
-             <Button 
-            title="Kullanıcı Adına Çevir"
-            onPress={ConvertUserName}
             
-            />
             <View style={{marginTop:15}}>
                 <Button 
             title="Çıkış Yap"
@@ -46,10 +84,30 @@ const MainPage = props => {
             
             />
             </View>
+            <FlatList
+            keyExtractor={(_,index)=>index.toString()}
+            data={postList}
+    renderItem={({item}) => <Text>deneme</Text>}
             
+            
+            />
            
             </View>
+            <View>
+            <TextInput
+            onChangeText={changePost}
+            placeholder="Post Giriniz"
+            
+            />
+            <Button 
+            title="Ekle"
+            onPress={pushData}
+            
+            />
+        </View>
         </SafeAreaView>
+
+      
         
     )
 }
